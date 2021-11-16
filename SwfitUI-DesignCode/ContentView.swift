19db3586b0c19f3ffd065c45fb36f3f974bc0659
Mirkan <mirkancaliskan@gmail.com>
2021-11-16 12:12:22 +0200

    Initial Commit

---
+++ SwfitUI-DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwfitUI-DesignCode
+//
+//  Created by Mirkan Çalışkan on 16.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwfitUI-DesignCode/SwfitUI_DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfitUI_DesignCodeApp.swift
+//  SwfitUI-DesignCode
+//
+//  Created by Mirkan Çalışkan on 16.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwfitUI_DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
