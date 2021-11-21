f02f6cee0da12ceda6d139a5ee66b727d18ecd28
Mirkan <mirkancaliskan@gmail.com>
2021-11-21 13:49:14 +0200

    add list actions

--- SwfitUI-DesignCode/UpdateList.swift
+++ SwfitUI-DesignCode/UpdateList.swift
@@ -8,43 +8,59 @@
 import SwiftUI
 
 struct UpdateList: View {
+    @ObservedObject var store = UpdateStore()
+    
+    func addUpdate() {
+        store.updates.append(Update(image: "Card1", title: "New Item", text: "text", date: "JAN 1"))
+    }
+    
     var body: some View {
-        
         NavigationView {
-            List(updateData) { update in
-                NavigationLink(destination: UpdateDetail(update: update)) {
-                    HStack {
-                        Image(update.image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 80, height: 80)
-                            .background(Color.black)
-                            .cornerRadius(20)
-                            .padding(.trailing, 4)
-                        
-                        VStack(alignment: .leading, spacing: 8) {
-                            Text(update.title)
-                                .font(.system(size: 20, weight: .bold))
-                            Text(update.text)
-                                .lineLimit(2)
-                                .font(.subheadline)
-                                .foregroundColor(Color(#colorLiteral(red: 0.6642242074, green: 0.6642400622, blue: 0.6642315388, alpha: 1)))
+            List {
+                ForEach(store.updates) { update in
+                    NavigationLink(destination: UpdateDetail(update: update)) {
+                        HStack {
+                            Image(update.image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 80, height: 80)
+                                .background(Color.black)
+                                .cornerRadius(20)
+                                .padding(.trailing, 4)
                             
-                            Text(update.date)
-                                .font(.caption)
-                                .fontWeight(.bold)
-                                .foregroundColor(.secondary)
-                            
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text(update.title)
+                                    .font(.system(size: 20, weight: .bold))
+                                Text(update.text)
+                                    .lineLimit(2)
+                                    .font(.subheadline)
+                                    .foregroundColor(Color(#colorLiteral(red: 0.6642242074, green: 0.6642400622, blue: 0.6642315388, alpha: 1)))
+                                
+                                Text(update.date)
+                                    .font(.caption)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.secondary)
+                                
+                            }
                         }
+                        .padding(.vertical, 8)
                     }
-                    .padding(.vertical, 8)
-                    
-                }.navigationBarTitle(Text("Updates"))
+                }
+                .onDelete { index in
+                    self.store.updates.remove(at: index.first!)
+                }
+                .onMove { (source: IndexSet, destination: Int) in
+                    self.store.updates.move(fromOffsets: source, toOffset: destination)
+                }
             }
-            
+            .navigationBarTitle(Text("Updates"))
+            .navigationBarItems(leading: Button(action: addUpdate) {
+                Text("Add Update")
+            }, trailing: EditButton())
         }
     }
 }
+
 
 struct UpdateList_Previews: PreviewProvider {
     static var previews: some View {
