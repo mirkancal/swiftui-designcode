a397a0f5da1d78ac21c56c942d2e21e78d36840a
Mirkan <mirkancaliskan@gmail.com>
2021-11-21 13:16:03 +0200

    add home

---
+++ SwfitUI-DesignCode/Home.swift
@@ -0,0 +1,73 @@
+//
+//  Home.swift
+//  SwfitUI-DesignCode
+//
+//  Created by Mirkan Çalışkan on 19.11.2021.
+//
+
+import SwiftUI
+
+struct Home: View {
+    @State var showProfile = false
+    @State var viewState = CGSize.zero
+    
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1))
+                .edgesIgnoringSafeArea(.all)
+            
+            HomeView(showProfile: $showProfile)
+                .padding(.top, 44)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+                .offset(y: showProfile ? -450 : 0)
+                .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
+                .scaleEffect(showProfile ? 0.9 : 1)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0), value: showProfile)
+                .edgesIgnoringSafeArea(.all)
+            
+            MenuView()
+                .background(Color.black.opacity(0.001))
+                .offset(y: showProfile ? 0 : screen.height)
+                .offset(y: viewState.height)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0), value: showProfile)
+                .onTapGesture {
+                    self.showProfile.toggle()
+                }
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.viewState = value.translation
+                    }.onEnded { value in
+                        if self.viewState.height > 50 {
+                            self.showProfile = false
+                        }
+                        self.viewState = .zero
+                        
+                    }
+                )
+            
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
+struct AvatarView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        Button(action:{ self.showProfile.toggle() }) {
+            Image("Avatar")
+                .resizable()
+                .frame(width: 36, height: 36)
+                .clipShape(Circle())
+        }
+    }
+}
+
+let screen = UIScreen.main.bounds
